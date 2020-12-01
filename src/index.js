4f2f9a0f786d8d0d3dbe35f3d41c672cee6087b5
Yegor Gangalo <gangaloyegor@gmail.com>
2020-12-02 01:35:39 +0200

    add img minimize

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
+
+import myCode from './js/mycodes';
+myCode();
+
+// import './images/_DSC1603.jpg';
+// import './images/milk.svg';
+// import './images/milk188@2x.png';
+require.context("./images/", true, /\.(png|svg|jpg|gif)$/); //вставляє усі картинки з ісходної папки в оброблену
+
 import './css/styles.scss';
-
-let qwerty = 1;
-
-const higherOrderFunction = () => {
-    let num = 'a';
-    const manyConsoleLogs = (vr) => {
-    num += vr;
-        qwerty += 2;
-        console.log(num);
-        console.log(qwerty);
-    };
-
-    return {
-        manyConsoleLogs
-    };
-};
-const func1 = higherOrderFunction();
-func1.manyConsoleLogs('b');
-func1.manyConsoleLogs('c');
-func1.manyConsoleLogs('d');
-func1.manyConsoleLogs('e');
-func1.manyConsoleLogs('f');
-
-const func2 = higherOrderFunction();
-func2.manyConsoleLogs('g');
-func2.manyConsoleLogs('h');
-
-/* -------------------------- */
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: './src/index.js', //шлях до точки входу відносно даного файлу налаштувань
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'//назва файлу, куди буде записано весь код з усіх файлів js
+        filename: '[name].[contenthash].js'
+        // filename: 'main.js'//назва файлу, куди буде записано весь код з усіх файлів js
     },
     module: {
         rules: [
@@ -18,6 +19,37 @@ module.exports = {
             {
               test: /\.s[ac]ss$/i,
               use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+              test: /\.(png|jpe?g|svg|gif)$/i,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                          name: '[name].[ext]',
+                          outputPath: 'images',
+                      },
+                    },
+                    {
+                      loader: 'image-webpack-loader',
+                      options: {
+                          mozjpeg: {
+                              progressive: true,
+                              quality: 70,
+                          },
+                          webp: {
+                            quality: 70,
+                          },
+                          optipng: {
+                            enabled: false,
+                          },
+                          pngquant: {
+                            quality: [0.65, 0.75],
+                            speed: 4
+                          },
+                      },
+                    }
+                ],
             },
         ],
     },
